433411b1da4efc8e78b52b328caf58459822caac
Peter Stuifzand <peter@stuifzand.eu>
2015-03-18 22:09:31 +0100

    Week 3 - Exercise 7 - Widget

---
+++ app/code/community/Peter/Week3/Block/Colorpicker.php
@@ -0,0 +1,7 @@
+<?php
+
+class Peter_Week3_Block_Colorpicker extends Mage_Core_Block_Template implements Mage_Widget_Block_Interface {
+    public function _toHtml() {
+        return "This is a test text";
+    }
+}
