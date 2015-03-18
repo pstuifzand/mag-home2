7687145abc8521196b1e0c6b65efbb380dc8764a
Peter Stuifzand <peter@stuifzand.eu>
2015-03-18 20:53:34 +0100

    Week 3 - Exercise 4
    In a custom action controller, render only the contents of a single
    custom update handle.

---
+++ app/code/community/Peter/Week3/controllers/IndexController.php
@@ -0,0 +1,10 @@
+<?php
+
+class Peter_Week3_IndexController extends Mage_Core_Controller_Front_Action  {
+
+    public function indexAction() {
+        $this->loadLayout();
+        $this->renderLayout();
+    }
+
+}
