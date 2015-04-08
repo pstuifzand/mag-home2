768cccfb4de8a729924ad2fb81bdf79ba6bfc584
Peter Stuifzand <peter.stuifzand@gmail.com>
2015-04-08 22:21:13 +0200

    Week 6 - Exercise 1 - Save and delete

--- app/code/community/Peter/Week3/controllers/Adminhtml/CommentsController.php
+++ app/code/community/Peter/Week3/controllers/Adminhtml/CommentsController.php
@@ -6,7 +6,6 @@ class Peter_Week3_Adminhtml_CommentsController extends Mage_Adminhtml_Controller
         $this->_title('Comments');
         $this->loadLayout();
         $this->_setActiveMenu('cms/comments');
-        $this->_addContent($this->getLayout()->createBlock('week3/adminhtml_comments'));
         $this->renderLayout();
     }
 
@@ -22,9 +21,49 @@ class Peter_Week3_Adminhtml_CommentsController extends Mage_Adminhtml_Controller
         $this->_setActiveMenu('cms/comments');
         $this->renderLayout();
     }
+
     public function newAction() {
         $this->loadLayout();
         $this->_setActiveMenu('cms/comments');
         $this->renderLayout();
     }
+
+    public function saveAction() {
+        if ($data = $this->getRequest()->getPost()) {
+            $model = Mage::getModel('week3/comment');
+            $model->setData($data)->setId($this->getRequest()->getParam('id'));
+
+            try {
+                $model->save();
+                Mage::getSingleton('adminhtml/session')->addSuccess(Mage::helper('peter_week3')->__('Item was successfully saved'));
+                Mage::getSingleton('adminhtml/session')->setFormData(false);
+
+                if ($this->getRequest()->getParam('back')) {
+                    $this->_redirect('*/*/edit', array('id' => $model->getId()));
+                    return;
+                }
+                $this->_redirect('*/*/');
+                return;
+            } catch (Exception $e) {
+                Mage::getSingleton('adminhtml/session')->addError($e->getMessage());
+                Mage::getSingleton('adminhtml/session')->setFormData($data);
+                $this->_redirect('*/*/edit', array('id' => $this->getRequest()->getParam('id')));
+                return;
+            }
+        }
+
+        Mage::getSingleton('adminhtml/session')->addError(Mage::helper('peter_week3')->__('Unable to find item to save'));
+        $this->_redirect('*/*/'); 
+    }
+
+    public function deleteAction() {
+        $id = $this->getRequest()->getParam('id');
+        $model = Mage::getModel('week3/comment');
+        $model->load($id);
+        $model->delete();
+        $this->_redirect('*/*/');
+        return;
+    }
+    
 }
+
