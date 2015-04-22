d95542ed18e05b9b3b4b4d44f6b38df59303fe05
Peter Stuifzand <peter.stuifzand@gmail.com>
2015-04-22 22:01:10 +0200

    Week 7 - Exercise 4 - Indexer update immediately

--- app/code/community/Peter/Week3/Model/Comment.php
+++ app/code/community/Peter/Week3/Model/Comment.php
@@ -1,8 +1,25 @@
 <?php
 
-class Peter_Week3_Model_Comment extends Mage_Core_Model_Abstract {
+class Peter_Week3_Model_Comment extends Mage_Core_Model_Abstract
+{
+    const ENTITY                 = 'week3_comment';
+
 	protected function _construct()
     {
         $this->_init('week3/comment');
+    }
+
+    protected function _afterSave()
+    {
+        Mage::getSingleton('index/indexer')->logEvent(
+            $this, self::ENTITY, Mage_Index_Model_Event::TYPE_SAVE
+        );
+    }
+
+    protected function _beforeDelete()
+    {
+        Mage::getSingleton('index/indexer')->logEvent(
+            $this, self::ENTITY, Mage_Index_Model_Event::TYPE_DELETE
+        );
     }
 }
--- app/code/community/Peter/Week3/Model/Indexer/Comment.php
+++ app/code/community/Peter/Week3/Model/Indexer/Comment.php
@@ -2,6 +2,16 @@
 
 class Peter_Week3_Model_Indexer_Comment extends Mage_Index_Model_Indexer_Abstract
 {
+    const EVENT_MATCH_RESULT_KEY = 'peter_week3_indexer_comment_match_result';
+
+    protected $_matchedEntities = array(
+        Mage_Catalog_Model_Product::ENTITY => array(
+            Mage_Index_Model_Event::TYPE_SAVE,
+            Mage_Index_Model_Event::TYPE_MASS_ACTION,
+            Mage_Index_Model_Event::TYPE_DELETE
+        )
+    );
+
 	protected function _construct()
     {
         $this->_init('week3/indexer_comment');
@@ -22,17 +32,20 @@ class Peter_Week3_Model_Indexer_Comment extends Mage_Index_Model_Indexer_Abstrac
         return 'Index week3 comments';
     }
 
-
     protected function _registerEvent(Mage_Index_Model_Event $event)
     {
+        Mage::log('_registerEvent');
     }
 
     protected function _processEvent(Mage_Index_Model_Event $event)
     {
+        Mage::log('_processEvent');
     }
 
     public function matchEvent(Mage_Index_Model_Event $event)
     {
+        Mage::log('matchEvent');
+        Mage::log($event);
         return false;
     }
 
