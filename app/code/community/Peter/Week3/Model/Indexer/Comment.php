f5f92b5297b2c4c11aee39e6c85011968efec72f
Peter Stuifzand <peter.stuifzand@gmail.com>
2015-04-22 21:50:31 +0200

    Week 7 - Exercise 3 - Integrate indexer

--- app/code/community/Peter/Week3/Model/Indexer/Comment.php
+++ app/code/community/Peter/Week3/Model/Indexer/Comment.php
@@ -7,8 +7,34 @@ class Peter_Week3_Model_Indexer_Comment extends Mage_Index_Model_Indexer_Abstrac
         $this->_init('week3/indexer_comment');
     }
 
-    public function rebuildIndex() {
+    public function reindexAll()
+    {
         $this->getResource()->rebuildIndex();
     }
+
+    public function getName()
+    {
+        return 'Week3 comment indexer';
+    }
+
+    public function getDescription()
+    {
+        return 'Index week3 comments';
+    }
+
+
+    protected function _registerEvent(Mage_Index_Model_Event $event)
+    {
+    }
+
+    protected function _processEvent(Mage_Index_Model_Event $event)
+    {
+    }
+
+    public function matchEvent(Mage_Index_Model_Event $event)
+    {
+        return false;
+    }
+
 }
 
