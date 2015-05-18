072c25049d4ca4da790baad8c778cf698f36ed88
Peter Stuifzand <peter.stuifzand@gmail.com>
2015-05-18 21:34:48 +0200

    Week 8 - Exercise 5 - Fix SOAP V2

--- app/code/community/Peter/Week3/Model/Online/Api/V2.php
+++ app/code/community/Peter/Week3/Model/Online/Api/V2.php
@@ -1,7 +1,4 @@
 <?php
 
 class Peter_Week3_Model_Online_Api_V2 extends Peter_Week3_Model_Online_Api {
-    public function onlineGet($message) {
-        return $message;
-    }
 }
--- client.php
+++ client.php
@@ -12,15 +12,17 @@ Mage::app();
 $myhellomsg = "<b>Helloworld Mydons</b>";
 
 try {
-$soap = new SoapClient('http://localhost/magento/api/?wsdl');
-$sessionId = $soap->login('apiUser', 'testtest12');
-//echo "Login ID : $sessionId";
-$result = $soap->call($sessionId, 'online.get', array());
-#$result = $soap->onlineGet($sessionId);
-echo $result;
+    $soap = new SoapClient('http://localhost/magento/api/v2_soap/?wsdl');
+    $sessionId = $soap->login('apiUser', 'testtest12');
+    //echo "Login ID : $sessionId";
+    #$result = $soap->call($sessionId, 'online.get', array());
+    $result = $soap->onlineGet($sessionId);
+    echo $result."\n";
+    $result = $soap->onlineGet($sessionId);
+    echo $result."\n";
 
 }
 catch(Exception $e) {
- echo $e->getMessage();
+    echo $e->getMessage();
 }
 
